833e744ee6da6c3f0942a13782b366343425b6e6
Raazia-26 <razi.raazia@gmail.com>
2023-10-31 16:01:48 +0100

    Added Local time zone

--- src/index.js
+++ src/index.js
@@ -28,11 +28,30 @@ updateTime();
 setInterval(updateTime, 1000);
 
 function showTime(event) {
+  if (event.target.value === "current_loc") {
+    let localtime = moment.tz.guess();
+    let localCity = localtime.split("/")[1].replace("_", " ");
+    let showcityTime = document.querySelector("#cities");
+    showcityTime.innerHTML = `<div class="city" >
+      <div>
+        <h2> ${localCity}
+          
+        </h2>
+        <div class="date">${moment().tz(localtime).format("dddd Do YYYY")}</div>
+      </div>
+      <div class="time">
+        ${moment().tz(localtime).format("hh:m:ss")} <small>${moment()
+      .tz(localtime)
+      .format("A")}</small>
+      </div>
+    </div>`;
+  }
+
   let selectCity = event.target.value;
   let cityName = selectCity.split("/")[1].replace("_", " ");
 
   let cityTime = moment().tz(selectCity);
-  let showcityTime = document.querySelector("#cities");
+  showcityTime = document.querySelector("#cities");
   showcityTime.innerHTML = `<div class="city" >
       <div>
         <h2> ${cityName}
@@ -41,7 +60,7 @@ function showTime(event) {
         <div class="date">${cityTime.format("dddd Do YYYY")}</div>
       </div>
       <div class="time">
-        ${cityTime.format("h:m:ss")} <small>${cityTime.format("A")}</small>
+        ${cityTime.format("hh:m:ss")} <small>${cityTime.format("A")}</small>
       </div>
     </div>`;
 }
